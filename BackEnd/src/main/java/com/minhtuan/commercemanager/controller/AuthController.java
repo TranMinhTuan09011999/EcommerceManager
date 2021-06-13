bb1e79798f5ada2de4b37afcde8a0f627775c0c4
TranMinhTuan09011999 <60993102+TranMinhTuan09011999@users.noreply.github.com>
2021-06-13 15:47:25 +0700

    Update AuthController.java

--- BackEnd/src/main/java/com/minhtuan/commercemanager/controller/AuthController.java
+++ BackEnd/src/main/java/com/minhtuan/commercemanager/controller/AuthController.java
@@ -52,6 +52,7 @@ public class AuthController {
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
 
