66c87acc330ccb0d69c40f4f3cce60515442f88c
TranMinhTuan09011999 <60993102+TranMinhTuan09011999@users.noreply.github.com>
2021-06-13 15:45:35 +0700

    Update AuthController.java

--- BackEnd/src/main/java/com/minhtuan/commercemanager/controller/AuthController.java
+++ BackEnd/src/main/java/com/minhtuan/commercemanager/controller/AuthController.java
@@ -76,6 +76,7 @@ public class AuthController {
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+        
         if (userRepository.existsByUsername(signUpRequest.getUsername())) {
             return ResponseEntity
                     .badRequest()
